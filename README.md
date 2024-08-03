88355a04fe9b038e771849ac255c9e8c13fa1804
Andrew McLachlan <andrew.mclachlan@me.com>
2024-08-03 10:27:37 +0100

    More complete test cases. Updated Web App.

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # playwright-python
+
 PyTest example using Playwright to test a website
+
+## Prerequisites
+
+- Python 3.x must be installed on your machine. You can download it from [python.org](https://www.python.org/downloads/).
+
+- Node.js 20.x or greater must be installed on your machine. You can use the instructions found on <https://nodejs.org/en/download/package-manager> to install Node.js.
+
+## Installation
+
+1. Clone the repository:
+
+   ```sh
+   git clone https://github.com/andrew-mclachlan/playwright-python.git
+   cd playwright-python
+   ```
+
+2. Create a virtual environment:
+
+   ```sh
+   python3 -m venv venv
+   source venv/bin/activate  # On Windows use `venv\Scripts\activate`
+   ```
+
+3. Install the required modules:
+
+   ```sh
+   pip install -r requirements.txt
+   ```
+
+## Build and run the Web App
+
+1. Install dependencies
+
+```sh
+   cd nextjs-blog
+   npm install
+```
+
+1. Launch the Web App
+
+```sh
+   npm run dev
+```
+
+## Running Tests
+
+To run the tests with `pytest`, open a terminal and use the following command:
+
+```sh
+pytest
+```
+
+## Debugging Tests
+
+Debugging Tests with VS Code
+Open the project in Visual Studio Code.
+Ensure you have the Python extension installed.
+Set breakpoints in your test code.
+Go to the Run and Debug view (Ctrl+Shift+D) and select "Python: Run Pytest".
+Click the green play button to start debugging.
+
+## Debugging Tests with Playwright UI
+
+```sh
+PWDEBUG=1 pytest -s e2e-tests/test_example.py
+```
--- e2e-tests/test_example.py
+++ e2e-tests/test_example.py
@@ -12,3 +12,51 @@ def test_example(browser):
     page = browser.new_page()
     page.goto("http://localhost:3000")
     assert page.title() == "Create Next App"
+
+def test_click_button(browser):
+    page = browser.new_page()
+    page.goto("http://localhost:3000")
+    assert page.title() == "Create Next App"
+    
+    click_me_button = page.query_selector('[data-testid="click-me"]')
+    click_me_button.click()
+    
+    greetings_element = page.query_selector('[data-testid="greetings"]')
+    assert greetings_element.inner_text() == "Hello World"
+
+def test_click_modal_and_close(browser):
+    page = browser.new_page()
+    page.goto("http://localhost:3000")
+    assert page.title() == "Create Next App"
+    
+    click_me_button = page.query_selector('[data-testid="click-me"]')
+    click_me_button.click()
+    
+    greetings_element = page.query_selector('[data-testid="greetings"]')
+    assert greetings_element.inner_text() == "Hello World"
+
+    close_button = page.query_selector('[data-testid="close-modal"]')
+    close_button.click()
+
+    click_me = page.query_selector('[data-testid="click-me"]')
+    assert click_me.inner_text() == "Click Me"
+
+def test_click_modal_enter_text_and_close(browser):
+    page = browser.new_page()
+    page.goto("http://localhost:3000")
+    assert page.title() == "Create Next App"
+    
+    click_me_button = page.query_selector('[data-testid="click-me"]')
+    click_me_button.click()
+    
+    greetings_element = page.query_selector('[data-testid="greetings"]')
+    assert greetings_element.inner_text() == "Hello World"
+
+    input_text = page.query_selector('[data-testid="entry-box"]')
+    input_text.fill("Welcome to Playwright")
+
+    close_button = page.query_selector('[data-testid="close-modal"]')
+    close_button.click()
+
+    message = page.query_selector('[data-testid="message"]')
+    assert message.inner_text() == "Welcome to Playwright"
